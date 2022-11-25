17b47381d6a16726b2b387e0015208838c8f5b05
mmogi03 <mmogilevsky03@gmail.com>
2022-11-25 18:20:15 -0500

    Deploy

---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    title: "Finance Web App",
+    subtitle: "Python, Flask, HTML, CSS, JavsScript",
+    description:
+      "Finance Web App that allows users to view, buy, and sell live-time stock through the IEX API.",
+    image: "./financeapp.png",
+    link: "https://github.com/mmogi03/MogiFinance",
+  },
+  {
+    title: "Path Finding Algorithm Visualizer App",
+    subtitle: "React.js, Node.js, JavaScript",
+    description:
+      "App that allows users to view animations of different path-finding algorithms including A*, Dijkstra's, DFS, and BFS.",
+    image: "./pathfing.png",
+    link: "https://github.com/mmogi03/PathFindingVisualizer",
+  },
+  {
+    title: "Road Lane Line Detection Algorithm",
+    subtitle: "Python, Computer Vision",
+    description:
+      "Computer Vision algorithm that detects road lane lines using Hough Transformations, Gaussian Blurring, Canny, and other techniques.",
+    image: "./lanedetection.png",
+    link: "https://github.com/mmogi03/LaneDetection",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborated in a team of 10-15 to design and prototype a ~1/15 scale autonomous car platform focused on precision and repeatability. I expanded on existing C/C++ microcontroller and embedded architectures, optimized Python script digital signal processing, implemented path-following algorithms, and developed a server-client infrastructure in Linux.",
+    image: "https://media-exp1.licdn.com/dms/image/C4E0BAQFxp9EzmG0z6A/company-logo_200_200/0/1519882416662?e=2147483647&v=beta&t=Ym12-9NeLCtGaBxfRw7CgBFiIH_Jh4dBDUJ6GjyLkUc",
+    name: "WINLAB, Rutgers - Wireless Information Network Laboratory",
+    company: "https://www.orbit-lab.org/wiki/Other/Summer",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Python",
+  "C",
+  "JavaScript",
+  "HTML/CSS",
+  "PowerShell",
+  "MATLAB",
+  "SQL",
+  "Linux",
+  "Jenkins",
+  "Flask",
+  "React.js",
+  "Node.js",
+  "ROS"
+];
